d85633907091ccfd8a930c7f0b49a05c9238f7b5
Letícia Castro <aicitelks@gmail.com>
2022-02-16 18:23:54 -0300

    Configurado o index.js para usar a camada de rotas

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const express = require('express');
+const cors = require('cors');
+const router = require('./routes/index');
+require('dotenv').config();
 
 const app = express();
 
 const PORT = process.env.PORT || 3001;
+
+app.use(cors());
+app.use(express.json());
+app.use(router);
 
 app.get('/', (_req, res) => {
   res.send('Boas vindas ao desafio Ebyrt!');

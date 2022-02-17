bf4869035aaf167e68e5ffeafc3549e39c30e2c1
Letícia Castro <aicitelks@gmail.com>
2022-02-17 11:50:56 -0300

    Implementada a rota de deletar a tarefa

--- routes/index.js
+++ routes/index.js
@@ -5,9 +5,11 @@ const router = express.Router();
 const {
   findAllController,
   insertTaskController,
+  deleteTaskController,
 } = require('../controllers/taskController');
 
 router.get('/tasks', findAllController);
 router.post('/tasks', insertTaskController);
+router.delete('/tasks/:id', deleteTaskController);
 
 module.exports = router;
